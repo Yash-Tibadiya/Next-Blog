0ff2c3adee8a1528e626056ac21137ee4d5f3252
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-11 18:26:38 +0530

    Implement Kinde authentication with AuthProvider and update Navbar for user session management

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect('/api/auth/register');
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
