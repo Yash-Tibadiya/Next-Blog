e86166dd5bd15d4bf924dd8834d846a5b3e03516
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-11 17:52:18 +0530

    Fetch blog posts from the database and display them on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import { prisma } from "./utils/db";
+
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      createdAt: true,
+    }
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <h1 key={item.title}>{item.title}</h1>
+        ))}
+      </div>
     </div>
   );
 }
