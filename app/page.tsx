5ddad85217638b7ce5d10395ee5329f31778f583
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-12 12:20:34 +0530

    "finnish caching in app" revalidatePath

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { prisma } from "./utils/db";
 import { Suspense } from "react";
 
+export const revalidate = 60;
+
 async function getData() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const data = await prisma.blogPost.findMany({
